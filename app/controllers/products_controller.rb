fda6e1fb7d5cd2fb4ca95af257abe681f5a8226b
RomainMaillot <romainmaillot.rm@gmail.com>
2020-02-05 16:02:23 +0000

    :sparkles: Add backoffice with rails admin

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.paginate(page: params[:page])
+    @products = Product.all
   end
 
   def new
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ActiveRecord::Base
     validates :name, :description, :price, presence: true
-    self.per_page = 10
     has_many :comments
 end
